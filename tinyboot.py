9e35de84b09c81b0d8b224e215690dcc8a959532
Kragen Javier Sitaker <kragen@pobox.com>
2008-10-03 23:09:05 -0300

    Initial checkin of the beginnings of a tiny compiler.

---
+++ tinyboot.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+"""Tiny bootstrapping interpreter for the first bootstrap stage.
+
+Implements an extremely minimal Forth-like language, used to write
+tinyboot1.tbf1.
+
+The theory is that first we 'compile' the program by reading through
+it to find compile-time definitions and actions, which sets up the
+initial state of memory; then we 'run' the program by directly
+interpreting its text, given that initial state.
+
+"""
+import sys, cgitb
+cgitb.enable(format='text')
+
+def debug(text):
+    sys.stderr.write(text + "\n")
+
+start_address = None
+memory = []                  # a list of bytes represented as integers
+
+### Compile-time actions.
+# Note that these should leave program_counter pointing after the
+# last byte they consume.
+
+program_counter = 0
+
+def eat_byte():
+    global program_counter
+    program_counter += 1
+def eat_comment():
+    while program[program_counter] != ')': eat_byte()
+    eat_byte()
+def advance_past_whitespace():
+    while program[program_counter] in ' \n': eat_byte()
+def push_dataspace_label(n):
+    return lambda: stack.append(n)
+def dataspace_label():
+    "Define a label in data space."
+    advance_past_whitespace()
+    name = program[program_counter]
+    run_time_dispatch[name] = push_dataspace_label(len(memory))
+    eat_byte()
+def read_number():
+    start = program_counter
+    while program[program_counter] in '0123456789': eat_byte()
+    return int(program[start:program_counter])
+def literal_byte():
+    advance_past_whitespace()
+    memory.append(read_number())
+def as_bytes(num):
+    "Convert a 32-byte number into a little-endian byte sequence."
+    return [num & 255, num >> 8 & 255, num >> 16 & 255, num >> 24 & 255]
+def literal_word():
+    "Compile a little-endian literal 32-byte number into data space."
+    advance_past_whitespace()
+    memory.extend(as_bytes(read_number()))
+def set_start_address():
+    global start_address
+    start_address = program_counter
+def nop(): pass
+
+compile_time_dispatch = {
+    '(': eat_comment,
+    'v': dataspace_label,
+    'b': literal_byte,
+    '#': literal_word,
+    '^': set_start_address,
+    ' ': nop, '\n': nop,
+}
+
+def tbfcompile():
+    while program_counter < len(program):
+        byte = program[program_counter]
+        eat_byte()
+        if byte in compile_time_dispatch:
+            compile_time_dispatch[byte]()
+        elif byte in run_time_dispatch:
+            pass                 # ignore things from run-time for now
+        else:
+            excerpt_beginning = max(0, program_counter - 10)
+            assert False, '%r not defined at %r (%r)' % \
+                   (byte, program[excerpt_beginning:program_counter], run_time_dispatch)
+
+### Run-time actions.
+# Execution should pretty much stay inside of functions, and we
+# shouldn't run into any compile-time actions there, right?
+# Except maybe comments.
+
+stack = []
+def write_out():
+    "Given an address and a count, write out some memory to stdout."
+    count = stack.pop()
+    address = stack.pop()
+    debug('writing address %d, count %d' % (address, count))
+    sys.stdout.write(''.join([chr(memory[ii])
+                              for ii in range(address, address+count)]))
+def quit():
+    sys.exit(0)
+def add():
+    stack.append(stack.pop() + stack.pop())
+def push_literal():
+    global program_counter
+    program_counter -= 1
+    stack.append(read_number())
+def decode(bytes):
+    return bytes[0] | bytes[1] << 8 | bytes[2] << 16 | bytes[3] << 24
+def fetch():
+    addr = stack.pop()
+    stack.append(decode(memory[addr:addr+4]))
+def store():
+    addr = stack.pop()
+    memory[addr:addr+4] = as_bytes(stack.pop())
+def bitwise_not():
+    stack.append(stack.pop() ^ 0xffffffff)
+
+run_time_dispatch = {    
+    '(': eat_comment,
+    'W': write_out,
+    'Q': quit,
+    '+': add,
+    ' ': nop, '\n': nop,
+    '@': fetch,
+    '!': store,
+    '~': bitwise_not,
+}
+for digit in '0123456789': run_time_dispatch[digit] = push_literal
+
+def tbfrun():
+    assert start_address is not None
+    global program_counter
+    program_counter = start_address
+    while True:
+        byte = program[program_counter]
+        eat_byte()
+        run_time_dispatch[byte]()
+
+def main():
+    global program
+    program = sys.stdin.read()
+    tbfcompile()
+    debug(str(memory))
+    tbfrun()
+    assert False, "tbfrun returned"
+
+if __name__ == '__main__': main()
---
+++ tinyboot.s
@@ -0,0 +1,41 @@
+	## sample code snippets for the tiny bootstrap forth compiler
+return: xchg %ebp, %esp
+        ret
+        ##  subroutine call
+call:   xchg %ebp, %esp
+        call foo
+        xchg %ebp, %esp
+        ##  push a constant
+push:   push %eax
+        mov $1234567, %eax
+fetch:  mov (%eax), %eax
+store:  pop (%eax)
+        pop %eax
+syscall:
+        pop %eax
+        pop %edx
+        pop %ecx
+        pop %ebx
+        int $0x80
+plus:   pop %ecx
+foo:    add %ecx, %eax
+lessthan:
+        sub (%esp), %eax
+        pop %eax
+        setge %al
+        dec %al
+        movsbl %al, %eax
+not:    not %eax
+bytefetch:
+        movzbl (%eax), %eax
+bytestore:
+        pop %ecx
+        movb %cl, (%eax)
+        pop %eax
+jump:   test %eax, %eax
+        pop %eax
+        jnz foo
+        jnz bar
+bar:    nop
+rshift: pop %ecx
+        sar %cl, %eax
