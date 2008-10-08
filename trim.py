6cb6c1bc384ddb89dd2f21e1c72eb8c2d988834e
Kragen Javier Sitaker <kragen@pobox.com>
2008-10-08 19:45:16 -0300

    Changed trim.py to respect existing newlines.

--- trim.py
+++ trim.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 "Remove comments and most extra whitespace from a tbf1 program."
 import sys
-wsp = False
-comment = False
+wsp = comment = newline = False
 while True:
     byte = sys.stdin.read(1)
     if not byte: break
     elif byte == '(': comment = True
     elif byte == ')': comment = False
-    elif not comment and byte in ' \n':
-        if not wsp: sys.stdout.write(byte)
-        wsp = True
     elif not comment:
-        sys.stdout.write(byte)
-        wsp = False
+        if byte == '\n': newline = True
+        elif byte == ' ': wsp = True
+        else:
+            if newline: sys.stdout.write('\n')
+            elif wsp: sys.stdout.write(' ')
+            sys.stdout.write(byte)
+            wsp = newline = False
+sys.stdout.write('\n')
