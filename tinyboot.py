62ede4cd6e502a6127561cbdd04ec03ab1555905
Aristotle Pagalztis <pagaltzis@gmx.de>
2008-10-08 21:36:57 +0200

    add some vertical whitespace in tinyboot.py

--- tinyboot.py
+++ tinyboot.py
@@ -19,7 +19,7 @@ def debug(text):
 
 start_address = None
 memory = []                  # a list of bytes represented as integers
-stack = []
+stack  = []
 rstack = []
 
 ### Compile-time actions.
@@ -31,50 +31,63 @@ program_counter = 0
 def eat_byte():
     global program_counter
     program_counter += 1
+
 def eat_comment():
     while program[program_counter] != ')': eat_byte()
     eat_byte()
+
 def advance_past_whitespace():
     while program[program_counter] in ' \n': eat_byte()
+
 def push_dataspace_label(n):
     return lambda: stack.append(n)
+
 def dataspace_label():
     "Define a label in data space."
     advance_past_whitespace()
     name = program[program_counter]
     eat_byte()
     run_time_dispatch[name] = push_dataspace_label(len(memory))
+
 def call_function(n):
     def rv():
         global program_counter
         rstack.append(program_counter)
         program_counter = n
     return rv
+
 def define_function():
     advance_past_whitespace()
     name = program[program_counter]
     eat_byte()
     run_time_dispatch[name] = call_function(program_counter)
+
 def read_number():
     start = program_counter
     while program[program_counter] in '0123456789': eat_byte()
     return int(program[start:program_counter])
+
 def literal_byte():
     advance_past_whitespace()
     memory.append(read_number())
+
 def as_bytes(num):
     "Convert a 32-byte number into a little-endian byte sequence."
     return [num & 255, num >> 8 & 255, num >> 16 & 255, num >> 24 & 255]
+
 def literal_word():
     "Compile a little-endian literal 32-byte number into data space."
     advance_past_whitespace()
     memory.extend(as_bytes(read_number()))
+
 def allocate_space():
     advance_past_whitespace()
     memory.extend([0] * read_number())
+
 def set_start_address():
     global start_address
     start_address = program_counter
+
 def nop(): pass
 
 # We have to find the backwards jump targets for loops while scanning
@@ -83,11 +96,15 @@ def nop(): pass
 # nest.
 
 jump_targets = {}
+
 def start_conditional():
     stack.append(program_counter)
+
 def end_conditional():
     jump_targets[stack.pop()] = program_counter
+
 start_loop = start_conditional
+
 def end_loop():
     jump_targets[program_counter] = stack.pop()
 
@@ -129,27 +146,36 @@ def write_out():
     debug('writing address %d, count %d' % (address, count))
     sys.stdout.write(''.join([chr(memory[ii])
                               for ii in range(address, address+count)]))
+
 def quit():
     sys.exit(0)
+
 def add():
     stack.append((stack.pop() + stack.pop()) & 0xFfffFfff)
+
 def push_literal():
     global program_counter
     program_counter -= 1
     stack.append(read_number())
+
 def decode(bytes):
     return bytes[0] | bytes[1] << 8 | bytes[2] << 16 | bytes[3] << 24
+
 def fetch():
     addr = stack.pop()
     stack.append(decode(memory[addr:addr+4]))
+
 def store():
     addr = stack.pop()
     memory[addr:addr+4] = as_bytes(stack.pop())
+
 def store_byte():
     addr = stack.pop()
     memory[addr] = stack.pop() & 255
+
 def bitwise_not():
     stack.append(stack.pop() ^ 0xFfffFfff)
+
 def less_than():
     b = stack.pop()
     a = stack.pop()
@@ -157,23 +183,28 @@ def less_than():
         stack.append(1)
     else:
         stack.append(0)
+
 def return_from_function():
     global program_counter
     program_counter = rstack.pop()
+
 def read_byte():
     byte = sys.stdin.read(1)
     if byte == '':
         stack.append(-1)
     else:
         stack.append(ord(byte))
+
 def conditional():
     if stack.pop(): return
     global program_counter
     program_counter = jump_targets[program_counter]
+
 def loop():
     if not stack.pop(): return
     global program_counter
     program_counter = jump_targets[program_counter]
+
 
 run_time_dispatch = {    
     '(': eat_comment,
@@ -192,7 +223,9 @@ run_time_dispatch = {
     '{': nop,         '}': loop,
     ' ': nop, '\n': nop,
 }
+
 for digit in '0123456789': run_time_dispatch[digit] = push_literal
+
 
 def tbfrun():
     assert start_address is not None
